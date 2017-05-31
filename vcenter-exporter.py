d52d3d9adf0c51f58a34d80ab1aabe2dfb0e80fb
Thomas Graichen <thomas.graichen@sap.com>
2017-05-31 12:51:29 +0200

    the listen port has to be an integer

--- vcenter-exporter.py
+++ vcenter-exporter.py
@@ -95,7 +95,7 @@ def main():
     counterids = perfManager.QueryPerfCounterByLevel(level=4)
 
     # start up the http server to expose the prometheus metrics
-    start_http_server(config.get('main').get('listen_port'))
+    start_http_server(int(config.get('main').get('listen_port')))
 
     logging.debug('list of all available metrics and their counterids')
     # loop over all counterids and build their full name and a dict relating it to the ids
